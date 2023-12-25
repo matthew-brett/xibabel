b9128465e1a5fff05a5bfe90d453aa1798271e24
Matthew Brett <matthew.brett@gmail.com>
2023-12-25 22:20:18 +0000

    Enable doctests in Sphinx build
    Though - we have no tests yet.

--- doc/conf.py
+++ doc/conf.py
@@ -14,7 +14,12 @@ release = '0.0.1a0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autosummary',
+    'numpydoc',
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
