c69ef1a5c00b0c0e49a09f418c0e791f14f5ffe2
Matthew Brett <matthew.brett@gmail.com>
2024-04-06 12:25:27 +0100

    Add same code line to example

--- example.py
+++ example.py
@@ -1,2 +1,9 @@
 a = [0, 1, 12, 3]
 print(max(*a))
+
+def f():
+    np = object()
+    nib_img = object()
+    slicers = []
+    new_affine = 0
+    assert np.all(nib_img.slicer[*slicers].affine == new_affine)
