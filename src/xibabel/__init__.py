1567d349d1d43df7f6b6602e9eb9ee28738dfe23
Matthew Brett <matthew.brett@gmail.com>
2024-04-06 23:47:48 +0100

    Updata init file

--- src/xibabel/__init__.py
+++ src/xibabel/__init__.py
@@ -1,4 +1,6 @@
-""" Init for Xibabel package
+""" Xibabel neuroimaging interface
+
+Xibabel is an experimental package for working with neuroimaging data formats.
 """
 
 from .loaders import load, save, to_nifti  # noqa:F401
