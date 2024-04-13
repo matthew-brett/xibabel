5bfffcdedd2f53f07dea14cf53236c6580655ead
Matthew Brett <matthew.brett@gmail.com>
2024-04-13 19:48:47 +0100

    Release candidate

--- src/xibabel/__init__.py
+++ src/xibabel/__init__.py
@@ -5,4 +5,4 @@ Xibabel is an experimental package for working with neuroimaging data formats.
 
 from .loaders import load, save, from_array, to_nifti  # noqa:F401
 
-__version__ = "0.0.1b4"
+__version__ = "0.0.1rc1"
