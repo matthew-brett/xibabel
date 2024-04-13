cf71b728279aaee4a863a2d1d1ea4e694cc844fa
Matthew Brett <matthew.brett@gmail.com>
2024-04-13 17:05:00 +0100

    Add from_array to public API

--- src/xibabel/__init__.py
+++ src/xibabel/__init__.py
@@ -3,6 +3,6 @@
 Xibabel is an experimental package for working with neuroimaging data formats.
 """
 
-from .loaders import load, save, to_nifti  # noqa:F401
+from .loaders import load, save, from_array, to_nifti  # noqa:F401
 
 __version__ = "0.0.1b4"
